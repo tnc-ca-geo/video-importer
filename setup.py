c3b04ac8888fae8e026baf38eca44f22c38bdecd
John Allard <john@jhallard.com>
2017-05-12 16:39:25 -0700

    Setting description and dependencies in setup script

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from setuptools import setup
 
 setup(name='import_video',
       version='0.1',
-      description="what is the description",
+      description="facilitates the importation and tracking of videos from a local directory to \
+              a segmentation/analysis web-server",
       author='camio.com',
       author_email='support.camio.com',
       url='https://github.com/tnc-ca-geo/video-importer',
-      install_requires=['hachoir_core','hachoir_parser','hachoir_metadata'],
+      install_requires=['hachoir_core','hachoir_parser','hachoir_metadata', 'csv', 'requests', 'psutil'],
       py_modules=["import_video"],
       license= 'BSD',
       package_data = {'': ['README.md']},
